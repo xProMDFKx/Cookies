973f7ca3210b01f59e951a3e6c6e7590d8d9418a
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-27 22:23:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,16 +27,43 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
   message.channel.sendEmbed(embed)
 }
 //BAN
-if (message.content.toLowerCase().startsWith('-ban')) {
+if (message.content.toLowerCase().startsWith('/ban')) {
     let member = message.mentions.members.first();
     let reason = message.content.split(' ').slice(2).join(' ');
-  if(!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) return message.channel.send("`Sorry, nu ai acces la aceasta comanda..`");
-  if(!member) return message.channel.send("**Ai uitat sa mentionezi cui vrei sa ii dai Ban!**");
+  if(!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) return message.channel.send("`Nu ai acces la aceasta comanda!`");
+  if(!member) return message.channel.send(":exclamation: ATENTIE, trebuie sa zici cui vrei sa ii dai ban");
   if(!member.kickable)  return message.channel.send("`Aceasta persoana nu poate primi ban!`");
   if(!reason) return message.channel.send("`Ai uitat sa pui motivul pentru care ii dai Ban lui " + member.tag + " !`");
   member.ban(reason)
   .catch(error => message.channel.send(`**Scuze ${message.author.tag} dar nu am putut sa dau Ban : ${error}**`));
   message.channel.send(`**${member.user.tag}** a fost banat de **${message.author.tag}** Motivul: ${reason}`);
+}
+//KICK
+if (message.content.toLowerCase().startsWith('/kick')) {
+    let member = message.mentions.members.first();
+    let reason = message.content.split(' ').slice(2).join(' ');
+  if(!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) return message.reply("**Sorry, dar nu ai acces la aceasta comanda.!**");
+  if(!member) return message.channel.send(":exclamation: ATENTIE, trebuie sa zici cui vrei sa ii dai kick");
+  if(!member.kickable)  return message.reply("**Aceasta persoana nu poate fi data afara de pe server!**");
+  if(!reason) return message.reply("**Ai uitat sa pui motivul pentru care ii dai Kick lui " + member.tag + " !**");
+  member.kick(reason)
+  .catch(error => message.channel.send(`**Scuze ${message.author.tag} dar nu am putut sa dau Kick din cauza unei erori : ${error}**`));
+  message.channel.send(`**${member.user.tag}** a primit kick de catre **${message.author.tag}** Motivul: ${reason}`);
+}
+//HELP
+  if (msg.content === '/help') {
+    msg.channel.send(':shield: ***Salut, eu am fost creat de @_xProMDFKx_#0957.Am fost creat ca sa ajut la moderarea serverului de discord.Eu detin comenzile: /ban, /kick, /delete, /forum si multe altele. Forumul comunitatii este : cookies-network.org  ***');
+  }
+//FORUM
+  if (msg.content === '/forum') {
+    msg.channel.send(':shield: ***Salut, forumul serverului este cookies-network.org***');
+  }
+//DELETE
+   if(args[1]) {
+     client.guilds.forEach(guild => {
+guild.members.forEach(member => {
+member.send(reason)
+message.delete()
 }
  
 });
