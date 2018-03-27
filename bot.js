f84f3b85012ff2b61cca183e350968d4f8ba0058
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-27 19:50:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,16 +11,17 @@ client.on('message', message => {
   	}
     
 //SERVERINFO
-if (message.content.startsWith(prefix / 'serverinfo')) {
-  const embed = new Discord.RichEmbed()
-  embed.addField('Membrii', message.guild.memberCount, true)
-  embed.addField('Nume', message.guild.name, true)
-  embed.addField('Regiunie', message.guild.region, true)
-  embed.addField('Fondator', message.guild.owner, true)
-  embed.addField('ID', message.guild.id, true)
-  embed.setColor(`ff0000`)
-  embed.setThumbnail(message.guild.iconURL)
-  message.channel.sendEmbed(embed)
+client.on('message', message => {
+    if (message.content === 'ping') {
+    const embed = new Discord.RichEmbed()
+    embed.addField('Membrii', message.guild.memberCount, true)
+    embed.addField('Nume', message.guild.name, true)
+    embed.addField('Regiunie', message.guild.region, true)
+    embed.addField('Fondator', message.guild.owner, true)
+    embed.addField('ID', message.guild.id, true)
+    embed.setColor(`ff0000`)
+    embed.setThumbnail(message.guild.iconURL)
+    message.channel.sendEmbed(embed)
 }
     
 });
