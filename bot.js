f38eb0c4fd2d5e6b07ed9f35f2d208f6c397ca2e
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-27 22:05:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const embed = new Discord.RichEmbed()
-const prefix = "%"
+const prefix = "/"
 client.on('message', message => {
   let args = message.content.split(" ").slice(1);
     
@@ -25,6 +25,18 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
   embed.setColor(`ff0000`)
   embed.setThumbnail(message.guild.iconURL)
   message.channel.sendEmbed(embed)
+}
+//BAN
+if (message.content.toLowerCase().startsWith('-ban')) {
+    let member = message.mentions.members.first();
+    let reason = message.content.split(' ').slice(2).join(' ');
+  if(!message.guild.member(message.author).hasPermission('BAN_MEMBERS')) return message.channel.send("`Sorry, nu ai acces la aceasta comanda..`");
+  if(!member) return message.channel.send("**Ai uitat sa mentionezi cui vrei sa ii dai Ban!**");
+  if(!member.kickable)  return message.channel.send("`Aceasta persoana nu poate primi ban!`");
+  if(!reason) return message.channel.send("`Ai uitat sa pui motivul pentru care ii dai Ban lui " + member.tag + " !`");
+  member.ban(reason)
+  .catch(error => message.channel.send(`**Scuze ${message.author.tag} dar nu am putut sa dau Ban : ${error}**`));
+  message.channel.send(`**${member.user.tag}** a fost banat de **${message.author.tag}** Motivul: ${reason}`);
 }
  
 });
