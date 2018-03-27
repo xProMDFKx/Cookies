61f3f135a5719f2fa17f07d6af39edeba15de842
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-27 17:51:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'ajutor') {
+    	message.reply('**Bun venit pe comunitatea noastra**');
+        message.reply('');
+        message.reply('**INFO**');
   	}
 });
 
