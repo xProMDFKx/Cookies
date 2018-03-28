a3a8765728803dbcb978650037705374c94379a0
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 07:46:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,25 +7,20 @@ const client = new Discord.Client();
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 }
-});
     
 client.on('message', message => {
     if (message.content === '/help') {
     	message.reply('***Creatorul meu este _xProMDFKx_#0957.Eu sunt creat pentru a modera servere de discord.Comenzile mele ! : /ban, /kick, /reload, /help, /forum  ***');
 }
-});
 
 client.on('message', message => {
     if (message.content === '/reload') {
     	message.reply('***RELOADIND CONFIG!! :ok_hand: DONE! ***');
 }
-});
 
 client.on('message', message => {
     if (message.content === '/forum') {
     	message.reply('Forumul nostru este ***cookies-network.org***');
 }
-});
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
