622eb16f1c57ac82705e75f12ec84eebd0a4395c
xProMDFKx <37838961+xProMDFKx@users.noreply.github.com>
2018-03-28 07:57:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const botconfig = require("./package.json");
-const tokenfile = require("./token.json");
-const Discord = require("discord.js");
+const Discord = require("bot.js");
 const fs = require("fs");
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
